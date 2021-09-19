c0e806588696c5a55bf92a8069a744bf0d1f8561
Maxim <masik2003410@gmail.com>
2021-09-19 21:31:43 +0300

    1st practic1

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,22 @@
 "use strict";
 
-
-let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
-console.log(numberOfFilms);
-
-
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
 let personalMovieDB ={
   count : numberOfFilms,
-  movies: {
-  },
-  actors: {
-  },
-
- genres: [],
- privat : false
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
 };
 
-const a = prompt("Один из последних просмоетрнных фильмов ?");
+const a = prompt("Один из последних просмотренных фильмов?", "");
 const b = prompt("Оцените этот фильм", "");
-const c = prompt("Один из последних просмоетрнных фильмов ?");
-const d = prompt("Оцените этот фильм", "");
+const c = prompt("Один из последних просмотренных фильмов?", "");
+const d = prompt("Оцените этот фильм?", "");personalMovieDB.movies[a] = b;
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-
 console.log(personalMovieDB);
+
