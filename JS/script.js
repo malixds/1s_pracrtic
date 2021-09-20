47a2a25e7970fe5556b6ce2b61be0c1ed4dd7d32
Maxim <masik2003410@gmail.com>
2021-09-20 19:18:21 +0300

    second practic

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,76 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
+console.log(numberOfFilms);
+
+
 
 let personalMovieDB ={
   count : numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
+  movies: {
+  },
+  actors: {
+  },
+
+ genres: [],
+ privat : false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("Оцените этот фильм", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = prompt("Оцените этот фильм?", "");personalMovieDB.movies[a] = b;
+// let i = 0
+// while (i < 2) {
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//   i ++;
+//     const a = prompt("Один из последних просмотренных фильмов ?", "");
+//      const b = prompt("Оцените этот фильм", "");
+
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//   personalMovieDB[a] = b;
+// }
+
+// else {
+//   i--;
+// }
+
+// }
+
+
+
+
+
+
+for (let i = 0; i <2; i++ ) {
+
+     const a = prompt("Один из последних просмотренных фильмов ?", "");
+     const b = prompt("Оцените этот фильм", "");
+
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+
+ personalMovieDB.movies[a] = b;
+}
+
+else {
+  i--;
+}
+
+}
+
+if (personalMovieDB.count < 10 ) {
+  alert("Просмотренно довольно мало фильмов");
+}
+
+else if (personalMovieDB.count >= 10 && numberOfFilms <= 30) 
+{
+  alert("Вы классический зритель");
+}
+
+else if (personalMovieDB.count > 30 ) {
+  alert("Ви кономан");
+}
+
+else {
+ alert ("Произошла ошибка");
+ console.log();
+}
 
 console.log(personalMovieDB);
-
